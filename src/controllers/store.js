aa68cbb4701fdffd1d77bdaca99be96f2cc7b64e
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-03-30 19:31:58 +0700

    feat: update api store

--- src/controllers/store.js
+++ src/controllers/store.js
@@ -37,8 +37,32 @@ export const list = async (req, res) => {
         });
     }
 
+    const start = req.query["_start"];
+    const limit = req.query["_limit"];
+
+    const filter = {};
+    const neArr = [];
+    const { _expand, _sort, _order, ...query } = req.query;
+    const queryArr = Object.keys(query);
+    for (let item of queryArr) {
+        if (item.includes("like")) {
+            filter[item.slice(0, item.indexOf("_"))] = new RegExp(req.query[item], "i");
+        } else if (item.includes("_ne")) {
+            neArr.push(item.slice(0, item.indexOf("_ne")), query[item]);
+        } else {
+            filter[item] = req.query[item];
+        }
+    }
+
     try {
-        const stores = await Store.find({}).select("-__v").sort(sortOpt).exec();
+        const stores = await Store
+            .find(filter)
+            .select("-__v")
+            .where(neArr[0] || " ").ne(neArr[1] || "")
+            .skip(start)
+            .limit(limit)
+            .sort(sortOpt)
+            .exec();
         res.json(stores);
     } catch (error) {
         res.status(400).json({
